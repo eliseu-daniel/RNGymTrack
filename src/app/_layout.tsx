9057668e265d170840be5fcebc68e904ec2f9f25
eliseu <eliseudaniel29@gmail.com>
2026-03-04 21:51:32 -0300

    fix: correction redirect start app

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,45 @@
-import { Stack } from "expo-router";
+import http from "@/service/HttpService";
+import { Stack, useRouter, useSegments } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
-  return (
-    <Stack screenOptions={
-      {
-        headerShown: false,
+  const router = useRouter();
+  const segments = useSegments();
+
+  useEffect(() => {
+    (async () => {
+      await http.ready();
+      const token = http.getToken();
+
+      const first = segments[0];
+      const isLogin = first === "index";
+
+      const protectedRoutes = [
+        "diet",
+        "dietSelected",
+        "home",
+        "training",
+        "trainingSelected",
+        "feedback",
+        "feedbackSelected",
+      ];
+
+      if (!token && protectedRoutes.includes(first)) {
+        router.replace("/index");
+        return;
       }
-    }/>
+
+      if (token && isLogin) {
+        router.replace("/home");
+        return;
+      }
+    })();
+  }, [segments]);
+
+  return (
+    <Stack
+      initialRouteName="index"
+      screenOptions={{ headerShown: false }}
+    />
   );
 }
